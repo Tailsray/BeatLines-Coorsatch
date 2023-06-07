c8a482235ece1abbcc41125a296c3a6588dedf22
Tailsray <57706893+Tailsray@users.noreply.github.com>
2023-06-08 01:36:52 +0300

    Restart reworked and JumpToBeat implemented

--- Kursovaya/retryButton.cs
+++ Kursovaya/retryButton.cs
@@ -3,24 +3,12 @@ using System;
 
 public partial class retryButton : TextureButton
 {
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		Disabled = false;
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		
-	}
-
 	public override void _Pressed()
-	
 	{
-		Visible = false;
-		GetTree().Paused = false;
-		GetTree().ReloadCurrentScene();
-		GetNode<CanvasModulate>("pauseBackground").Visible = false;
+		GetParent<Game>().ReloadConductor(0);
 	}
 }
