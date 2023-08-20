bced78786eea43057f7318677f866feddae4a39d
Tailsray <egorino@yandex.ru>
2023-08-20 06:30:05 +0300

    Go backkkkk
    ouch

--- Follower.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-public partial class Follower : Node2D
-{	
-	public override void _Draw()
-	{
-		DrawArc(new Vector2(), 27.5f, 0, Mathf.Tau, 100, Colors.Beige, 5, true);
-	}
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# BeatLines-Coorsatch
-Desktop, Godot 4.0.2 Mono, C#
